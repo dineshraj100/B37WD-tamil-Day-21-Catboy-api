fcfdc1accc7d09c39a0367cc9f7a9138b925e818
dineshraj100 <dineshrajmech01@gmail.com>
2023-05-09 13:59:49 +0530

    B37WD tamil Day 21 Catboy api (redo task)

---
+++ script.js
@@ -0,0 +1,23 @@
+async function foo(){
+    try{
+    let res=await fetch("https://api.catboys.com/img");
+    let res1=await res.json();
+    console.log(res1);
+    
+
+    
+    var ele=document.getElementById("img")
+    ele.innerHTML= `<div class="card" style="width: 300px;">
+    <img src="${res1.url}" class="card-img-top" alt="...">
+    <div class="card-body">
+    </div>
+  </div>`
+    }catch(error){
+        console.log(error);
+    }
+}
+    foo();
+
+
+
+
